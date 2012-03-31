84ca6a5f207c675e7bdc5cc8d3bf1e0a8c110ac3
Lucas Baudin <xapantu@gmail.com>
2012-03-31 10:57:51 +0200

    Refactoring

--- pythonmail.py
+++ pythonmail.py
@@ -9,6 +9,7 @@ from time import strftime
 import sqlite3
 import imaplib
 from contextlib import closing
+import chardet
 
 
 app = Flask(__name__)
@@ -23,6 +24,197 @@ app.config.from_object(__name__)
 
 imap_accounts = {}
 
+class EmailAccount(object):
+
+    def __init__(self, host, name, password, database_name):
+        self.host = host
+        self.name = name
+        self.password = password
+        self.database_name = database_name
+        self.load_imap_account()
+    
+    def load_imap_account(self):
+        mail = imaplib.IMAP4_SSL(self.host)
+        mail.login(self.name, self.password)
+        mail.list()
+        app.logger.debug("load imap accounts")
+        self.imap_mail = mail
+
+    def open_db(self):
+        self.db = sqlite3.connect(self.database_name)
+
+    def close_db(self):
+        self.db.close()
+    
+    def close_imap_account(self):
+        mail.close()
+        app.logger.debug("close imap")
+        mail.logout()
+
+    def load_mailbox(self, mailbox):
+        try:
+            self.imap_mail.select(mailbox) # connect to inbox.
+        except imaplib.abort: # maybe a timeout?
+            self.load_imap_account()
+            self.imap_mail.select("inbox") # connect to inbox.
+
+    def get_ns(self):
+        return self.name + ":" + self.host
+
+    """
+    Open the DB, check what is the oldest message we have there, and download all the new ones.
+    """
+    def load_messages(self):
+        cur = self.db.execute('select imapid from mails'
+                             + ' where account = \'' + self.get_ns() + '\''
+                             + ' order by imapid desc limit 1')
+        
+        last_imapid = 1
+        last_entry = cur.fetchall()
+        if len(last_entry) > 0: # if the database is empty, it can be 0
+            last_imapid = last_entry[0][0] + 1
+        else:
+            app.logger.debug("Database empty")
+        app.logger.debug("Fetch mails from: " + str(last_imapid))
+
+        self.download_messages(last_imapid)
+
+    """
+    Put the content of the massage in the db if it is not already there, and returns it.
+    """
+    def _load_message_body(self, imapid):
+        typ, data = self.imap_mail.uid("fetch", imapid, '(body.peek[])')
+        msg = data[0][1].decode(chardet.detect(data[0][1])["encoding"])
+        self.db.executemany("update mails set fulltext = ? where imapid = %s" % (imapid), [(msg,)])
+        self.db.commit()
+        return msg
+
+    """
+    Return a dict with the message values (e.g. subject, sender, body...)
+    """
+    def load_message(self, imapid):
+        cur = self.db.execute('select imapid, fulltext, encoding, subject, sender, seen from mails'
+                             + ' where account = \'' + self.get_ns() + '\''
+                             + ' and imapid = ' + str(imapid))
+        
+        entry = cur.fetchall()
+        if len(entry) is 0:
+            self.download_messages(imapid)
+            cur = self.db.execute('select imapid from mails'
+                                 + ' where account = \'' + self.get_ns() + '\''
+                                 + ' and imapid = ' + str(imapid))
+            
+            entry = cur.fetchall()
+            if len(entry) is 0:
+                raise
+        message = {}
+        message["imapid"] = entry[0][0]
+        message["fulltext"] = entry[0][1]
+        message["encoding"] = entry[0][2]
+        message["subject"] = entry[0][3]
+        message["sender"] = entry[0][4]
+        message["seen"] = True
+
+        encoding = "utf-8" # is this encoding stuff *really* necessary?
+        if message["fulltext"] is not None: # yay, we even have the content!!
+            if message["encoding"] is not None:
+                encoding = message["encoding"]
+        else:
+            message["fulltext"] = self._load_message_body(imapid)
+            encoding = chardet.detect(message["fulltext"])["encoding"]
+        email_message = email.message_from_string(message["fulltext"].encode(encoding))
+        content = get_content_from_message(email_message)
+        message["body"] = content
+        return message
+
+    def download_messages(self, start):
+        app.logger.debug("DONWLOAD")
+        typ, data = self.imap_mail.uid("fetch", str(start) + ":*",
+                             '(body.peek[header.fields (subject message-id from)] x-gm-thrid flags)')
+        app.logger.debug("Fetch from %s to *" % (start))
+        i = 0
+        final = "/" + str(len(data))
+        for msg in data:
+            if msg[0] == ")":
+                continue
+            email = {}
+
+            # Parse the email values:
+            values_split = msg[0].replace("(", "").split()
+            uid_index = values_split.index("UID")
+            thrid_index = values_split.index("X-GM-THRID")
+            message_id = values_split[uid_index+1]
+            thrid = values_split[thrid_index+1]
+            app.logger.debug(str(message_id) + final)
+
+            if int(message_id) < start:
+                continue
+
+            # Parse the flags
+            seen = "\\Seen" in imaplib.ParseFlags(msg[0])
+            email["seen"] = seen
+
+            parser = HeaderParser()
+            header = parser.parsestr(msg[1])
+
+            decoded = decode_header(header["subject"])[0]
+            encodage = "utf-8"
+            if decoded[1] is not None: encodage = decoded[1]
+            try:
+                subject = decoded[0].decode(encodage)
+            except UnicodeDecodeError:
+                subject = decoded[0].decode(chardet.detect(decoded[0])["encoding"])
+
+            email["imapid"] = message_id
+            email["subject"] = subject
+            email["sender"] = decode_header(header["from"])[0]
+            encodage = "utf-8"
+            if email["sender"][1] is not None: encodage = email["sender"][1]
+            try:
+                email["sender"] = email["sender"][0].decode(encodage)
+            except UnicodeDecodeError:
+                email["sender"] = email["sender"][0].decode(chardet.detect(email["sender"][0])["encoding"])
+            """
+            fulltext = ""
+            try:
+                fulltext = msg[1].decode(chardet.detect(msg[1])["encoding"])
+            except:
+                try:
+                    fulltext = msg[1].decode("utf-8") # sometimes, chardet is just false
+                except:
+                    fulltext = unicode(msg[1], errors="ignore") # ok, let's forgot odd chars
+            """
+            self.db.execute('insert into mails (subject, account, imapid, seen, sender, thrid)'
+                          + ' values (?, ?, ?, ?, ?, ?)',
+                             [email["subject"], self.get_ns (), email["imapid"], seen, email["sender"], thrid])
+            i += 1
+            if i > 500:
+                self.db.commit()
+                i = 0
+            cur = self.db.execute('select imapid from threads'
+                                + ' where imapid = \'' + str(thrid) + '\''
+                                + ' and account = \'' + self.get_ns() + '\''
+                                + ' limit 1')
+            if len(cur.fetchall()) is 0:
+                # We need to add a new thread
+                self.db.execute('insert into threads (subject, imapid, account)'
+                              + ' values (?, ?, ?)',
+                                [email["subject"], thrid, self.get_ns()])
+        self.db.commit()
+    
+    """
+    Return a list with all message from start to end.
+    0 is the last email
+    100 is the 100th most recent email
+    """
+    def load_list(self, start, end):
+        cur = self.db.execute('select imapid, subject, sender, seen from mails'
+                             + ' where account = \'' + self.get_ns() + '\''
+                             + ' order by imapid desc limit ' + str(end))
+        entries = [dict(imapid=row[0], subject=row[1], sender=row[2], seen=row[3]) for row in cur.fetchall()]
+        return entries
+
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -33,17 +225,17 @@ def init_db():
         db.commit()
             
 
-def load_imap_account(host, name, password):
-    mail = imaplib.IMAP4_SSL(host)
-    mail.login(name, password)
-    mail.list()
-    app.logger.debug("load imap")
-    imap_accounts[name] = mail
+#def load_imap_account(host, name, password):
+#    mail = imaplib.IMAP4_SSL(host)
+#    mail.login(name, password)
+#    mail.list()
+#    app.logger.debug("load imap")
+#    imap_accounts[name] = mail
 
-def close_imap_account():
-    mail.close()
-    app.logger.debug("close imap")
-    #mail.logout()
+#def close_imap_account():
+#    mail.close()
+#    app.logger.debug("close imap")
+#    #mail.logout()
 
 def load_message(mail, mails_id, start, end, database):
     if start > end:
@@ -218,55 +410,15 @@ def view_mail_raw(imapid, threaded = False, even = False):
     if not session.has_key("email"):
         return redirect("/")
     else:
-        # First - is it in the DB?
-        database = connect_db()
-        cur = database.execute('select imapid, subject, seen, fulltext, encoding, sender from mails'
-                               + ' where account = \'' + session["email"] + '\' and imapid = ' + str(imapid))
-        entries = [dict(imapid=row[0], subject=row[1],
-                        seen=row[2], fulltext=row[3], encoding=row[4], sender=row[5]) for row in cur.fetchall()]
-        import chardet
-        if len(entries) > 0: # yay, we have it in the db
-            message = entries[0]
-            encoding = "utf-8" # is this encoding stuff *really* necessary?
-            if message["fulltext"] is not None: # yay, we even have the content!!
-                if message["encoding"] is not None:
-                    encoding = message["encoding"]
-            else:
-                if not app.config["OFFLINE"]:
-                    if not imap_accounts.has_key(session["email"]):
-                        load_imap_account(session["host"], session["email"], session["password"])
-                    mail = imap_accounts[session["email"]]
-                    try:
-                        mail.select("inbox") # connect to inbox.
-                    except imaplib.abort:
-                        load_imap_account(session["host"], session["email"], session["password"])
-                        mail = imap_accounts[session["email"]]
-                        mail.select("inbox") # connect to inbox.
-                typ, data = mail.uid("fetch", imapid, '(body.peek[])')
-                msg = data[0][1].decode(chardet.detect(data[0][1])["encoding"])
-                app.logger.debug(chardet.detect(data[0][1]))
-                database.executemany("update mails set fulltext = ? where imapid = %s" % (imapid), [(msg,)])
-                database.commit()
-                database.close()
-                message["fulltext"] = msg
-                encoding = chardet.detect(data[0][1])["encoding"]
-            email_message = email.message_from_string(message["fulltext"].encode(encoding))
-            content = get_content_from_message(email_message)
-            message_tpl = {}
-            message_tpl["body"] = content
-            message_tpl["subject"] = message["subject"]
-            message_tpl["sender"] = message["sender"]
-            if threaded:
-                return (render_template("message.html", message=message_tpl, even=even), message_tpl)
-            else:
-                return render_template("message.html", message=message_tpl, even=even)
-        else:
-            mail = get_mail()
-            mails_id = {} # not used here, just a simple dict to send to load_message, useless in our case
-            load_message(mail, mails_id, imapid, imapid, database)
-            database.commit()
-            database.close()
-            return view_mail_raw(imapid)
+        if not email_accounts.has_key(session["email"]):
+            email_accounts[session["email"]] = EmailAccount(session["host"], session["email"], session["password"], "email.db")
+
+        mail = email_accounts[session["email"]]
+        mail.open_db()
+        mail.load_mailbox("inbox")
+        message = mail.load_message(imapid)
+        mail.close_db()
+        return render_template("message.html", message=message, even=False)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -353,6 +505,23 @@ def get_mail():
         mail.select("inbox") # connect to inbox.
     return mail
 
+email_accounts = {}
+@app.route("/see")
+def see():
+    if not session.has_key("email"):
+        return "not logged"
+    else:
+        if not email_accounts.has_key(session["email"]):
+            email_accounts[session["email"]] = EmailAccount(session["host"], session["email"], session["password"], "email.db")
+
+        mail = email_accounts[session["email"]]
+        mail.open_db()
+        mail.load_mailbox("inbox")
+        mail.load_messages()
+        mails_id = mail.load_list(0, 100)
+        mail.close_db()
+        emails_list = sorted(mails_id, lambda x, y: cmp(int(y["imapid"]), int(x["imapid"])))
+        return render_template('email-list.html', page=0, emails=emails_list)
 
 @app.route("/cache_all")
 def cache_all():

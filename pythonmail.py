16a540237f1c324d07cc2074107638086f5999ec
xapantu <xapantu@gmail.com>
2012-04-08 12:51:57 +0200

    Python2

--- pythonmail.py
+++ pythonmail.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, session, request, redirect, url_for, jsonify
--- reset_db.py
+++ reset_db.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 import os
 import pythonmail
 os.system("rm email.db")

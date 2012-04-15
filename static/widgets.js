60c09eeebc41765d3ed305ee1dd252cf86c821ba
xapantu <xapantu@gmail.com>
2012-04-14 17:10:25 -0700

    More widgets things, redesign the  settings popover

--- static/widgets.js
+++ static/widgets.js
@@ -3,4 +3,15 @@ $(function () {
         $(this).parent().children("a").removeClass("active");
         $(this).addClass("active");
     });
+    $(".staticnotebook > .modebutton > a").click(function(object) {
+    /* Hide all the panes */
+    $(this).parent().parent().children(".static-frame").hide();
+    $(this).parent().parent().children(".static-" + $(this).parent().children().index($(this))).show();
+    });
 });
+function popup_block(id) {
+    $("#" + id + ".popup-background").css("display", "table");
+}
+function close_popup(id) {
+    $("#" + id + ".popup-background").css("display", "none");
+}
